1323eded976a83fe90686184b7618b8700399e48
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-10-15 16:10:26 -0400

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,16 @@
 // Assignment code here
 
+// Variable Values 
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+// Number Values 
+number = [1,2,3,4,5,6,7,8,9,0]
+// Letter Values 
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 // Write password to the #password input
 function writePassword() {
