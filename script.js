9aa1c9dca0e97e5a01a19113b73f03d529d854e7
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-10-15 17:25:33 -0400

    initial validation sequence

--- script.js
+++ script.js
@@ -10,30 +10,30 @@ alphaLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q
 alphaUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 // Start Function to generate password
-function generatePassword(){
- // ask for user for a number between 8 & 128 
-
-  // if user doesn't input a number between 8 & 128 need to ask for validation 
-
-  // continue to user inputted variable validation 
-  // number
-  // character 
-  // alpha lowercase 
-  // alpha uppercase 
-
-  // if user doesn't choose require validation 
-
-
-
-}
- 
-
-
-
-
+let generatePassword = function(){
+  //ask user how long they'd like their password
+  let enter = window.prompt("How many characters would you like your password? Choose between 8 and 128.");
+    
+  //begin validation of statement
+    if (enter === "" || enter === null){
+      window.alert("You need to provide a valid answer. Please try again.");
+      return generatePassword();
+    }
+    if (enter >= 8 && enter <= 128){
+      let confirmNumber = window.confirm ("Will this contain numbers?");
+      let confirmCharacter = window.confirm ("Will this contain special characters?");
+      let confirmAlphaLower= window.confirm ("Will this contain lowercase letters?");
+      let confirmAlphaUpper = window.confirm ("Will this contain uppercase letters");
+    } else {
+      window.alert("You need to provide a number between 8 and 128. Please try again.");
+      return generatePassword();
+    }
+ };
 
 
 
+
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
